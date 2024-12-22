0a723ed8a4e06560709ebdb5e820edcd6fbe31b9
Serhij S <s@g5.is>
2024-12-22 23:35:31 +0100

    import

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# atomic-timer
-Atomic timer for Rust
+<h2>
+  atomic-timer - an atomic timer for Rust
+  <a href="https://crates.io/crates/atomic-timer"><img alt="crates.io page" src="https://img.shields.io/crates/v/atomic-timer.svg"></img></a>
+  <a href="https://docs.rs/atomic-timer"><img alt="docs.rs page" src="https://docs.rs/atomic-timer/badge.svg"></img></a>
+  <a href="https://github.com/roboplc/atomic-timer/actions/workflows/ci.yml">
+    <img alt="GitHub Actions CI" src="https://github.com/roboplc/atomic-timer/actions/workflows/ci.yml/badge.svg"></img>
+  </a>
+</h2>
+
+A passive timer object which can be manipulated atomically. Useful for
+automation and robotics tasks.
+
+Atomic timer is a part of [RoboPLC](https://www.roboplc.com) project.
+
+## Usage example
+
+### Basic usage
+
+```rust
+use atomic_timer::AtomicTimer;
+use std::time::Duration;
+
+let timer = AtomicTimer::new(Duration::from_secs(1));
+for _ in 0..100 {
+    if timer.expired() {
+        println!("Timer expired");
+        timer.reset(); // does not need to be mutable
+    } else {
+        println!("Elapsed: {:?}, remaining: {:?}", timer.elapsed(), timer.remaining());
+    }
+    // do some work
+}
+```
+
+### Multi-threaded usage
+
+```rust
+use atomic_timer::AtomicTimer;
+use std::sync::Arc;
+use std::time::Duration;
+
+let timer = Arc::new(AtomicTimer::new(Duration::from_secs(1)));
+for _ in 0..10 {
+    let timer = timer.clone();
+    std::thread::spawn(move || {
+        for _ in 0..100 {
+            if timer.reset_if_expired() {
+                println!("Timer expired");
+                // react to the timer expiration
+                // guaranteed to be true only for one thread
+            }
+            // do some other work
+        }
+    });
+}
+```
+
+## Serialization / deserialization
+
+Atomic timer objects can be safely serialized and de-serialized (requires
+`serde` feature).
+
+When a timer is de-serialized, it keeps its state (elapsed/remaining time),
+despite the system monotonic clock difference.
+
+## Limitations
+
+Currently does not works on WASM (can be added, write an issue if you really need it).
+
+## MSRV
+
+1.68.0
---
+++ src/lib.rs
@@ -0,0 +1,261 @@
+#![ doc = include_str!( concat!( env!( "CARGO_MANIFEST_DIR" ), "/", "README.md" ) ) ]
+#![deny(missing_docs)]
+use std::{
+    sync::atomic::{AtomicI64, Ordering},
+    time::Duration,
+};
+
+use bma_ts::Monotonic;
+
+/// Atomic timer
+pub struct AtomicTimer {
+    duration: i64,
+    start: AtomicI64,
+    monotonic_fn: fn() -> i64,
+}
+
+fn monotonic_ns() -> i64 {
+    i64::try_from(Monotonic::now().as_nanos()).expect("Monotonic time is too large")
+}
+
+impl AtomicTimer {
+    #[allow(dead_code)]
+    fn construct(duration: i64, elapsed: i64, monotonic_fn: fn() -> i64) -> Self {
+        AtomicTimer {
+            duration,
+            start: AtomicI64::new(monotonic_fn().saturating_sub(elapsed)),
+            monotonic_fn,
+        }
+    }
+    /// Create a new atomic timer
+    ///
+    /// # Panics
+    ///
+    /// Panics if the duration is too large (in nanos greater than `i64::MAX`)
+    pub fn new(duration: Duration) -> Self {
+        AtomicTimer {
+            duration: duration
+                .as_nanos()
+                .try_into()
+                .expect("Duration is too large"),
+            start: AtomicI64::new(monotonic_ns()),
+            monotonic_fn: monotonic_ns,
+        }
+    }
+    /// Reset the timer
+    #[inline]
+    pub fn reset(&self) {
+        self.start.store((self.monotonic_fn)(), Ordering::SeqCst);
+    }
+    /// Reset the timer if it has expired, return true if reset
+    #[inline]
+    pub fn reset_if_expired(&self) -> bool {
+        let now = (self.monotonic_fn)();
+        self.start
+            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |start| {
+                (now.saturating_sub(start) >= self.duration).then_some(now)
+            })
+            .is_ok()
+    }
+    /// Get the elapsed time
+    #[inline]
+    pub fn elapsed(&self) -> Duration {
+        Duration::from_nanos(
+            (self.monotonic_fn)()
+                .saturating_sub(self.start.load(Ordering::SeqCst))
+                .try_into()
+                .unwrap_or_default(),
+        )
+    }
+    /// Get the remaining time
+    #[inline]
+    pub fn remaining(&self) -> Duration {
+        let elapsed = self.elapsed_ns();
+        if elapsed >= self.duration {
+            Duration::default()
+        } else {
+            Duration::from_nanos((self.duration - elapsed).try_into().unwrap_or_default())
+        }
+    }
+    fn elapsed_ns(&self) -> i64 {
+        (self.monotonic_fn)().saturating_sub(self.start.load(Ordering::SeqCst))
+    }
+    /// Check if the timer has expired
+    #[inline]
+    pub fn expired(&self) -> bool {
+        self.elapsed_ns() >= self.duration
+    }
+}
+
+#[cfg(feature = "serde")]
+mod ser {
+    use super::{monotonic_ns, AtomicTimer};
+    use serde::{ser::SerializeTuple as _, Deserialize, Deserializer, Serialize, Serializer};
+
+    impl Serialize for AtomicTimer {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            // serialize duration and elapsed into map
+            let mut t = serializer.serialize_tuple(2)?;
+            t.serialize_element(&self.duration)?;
+            t.serialize_element(&self.elapsed_ns())?;
+            t.end()
+        }
+    }
+
+    impl<'de> Deserialize<'de> for AtomicTimer {
+        fn deserialize<D>(deserializer: D) -> Result<AtomicTimer, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            // deserialize duration and elapsed from map
+            let (duration, elapsed) = <(i64, i64)>::deserialize(deserializer)?;
+            Ok(AtomicTimer::construct(duration, elapsed, monotonic_ns))
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::AtomicTimer;
+    use std::{
+        sync::{Arc, Barrier},
+        thread,
+        time::Duration,
+    };
+
+    #[test]
+    fn test_reset() {
+        let timer = AtomicTimer::new(Duration::from_secs(5));
+        thread::sleep(Duration::from_secs(1));
+        timer.reset();
+        assert!(timer.elapsed() < Duration::from_millis(100));
+    }
+
+    #[test]
+    fn test_reset_if_expired() {
+        let timer = AtomicTimer::new(Duration::from_secs(1));
+        assert!(!timer.reset_if_expired());
+        thread::sleep(Duration::from_millis(1100));
+        assert!(timer.expired());
+        assert!(timer.reset_if_expired());
+    }
+
+    #[test]
+    fn test_reset_if_expired_no_datarace() {
+        let n = 1000;
+        let timer = Arc::new(AtomicTimer::new(Duration::from_millis(100)));
+        thread::sleep(Duration::from_millis(200));
+        assert!(timer.expired());
+        let barrier = Arc::new(Barrier::new(n));
+        let (tx, rx) = std::sync::mpsc::channel::<bool>();
+        let mut result = Vec::with_capacity(n);
+        for _ in 0..n {
+            let timer = timer.clone();
+            let barrier = barrier.clone();
+            let tx = tx.clone();
+            thread::spawn(move || {
+                barrier.wait();
+                tx.send(timer.reset_if_expired()).unwrap();
+            });
+        }
+        drop(tx);
+        while let Ok(v) = rx.recv() {
+            result.push(v);
+        }
+        assert_eq!(result.len(), n);
+        assert_eq!(result.into_iter().filter(|&v| v).count(), 1);
+    }
+}
+
+#[cfg(feature = "serde")]
+#[cfg(test)]
+mod test_serialization {
+    use super::AtomicTimer;
+    use std::{thread, time::Duration};
+
+    fn in_time_window(a: Duration, b: Duration, window: Duration) -> bool {
+        let diff = window / 2;
+        let min = b - diff;
+        let max = b + diff;
+        a >= min && a <= max
+    }
+
+    #[test]
+    fn test_serialize_deserialize() {
+        let timer = AtomicTimer::new(Duration::from_secs(5));
+        thread::sleep(Duration::from_secs(1));
+        let serialized = serde_json::to_string(&timer).unwrap();
+        let deserialized: AtomicTimer = serde_json::from_str(&serialized).unwrap();
+        assert!(in_time_window(
+            deserialized.elapsed(),
+            Duration::from_secs(1),
+            Duration::from_millis(100)
+        ));
+    }
+
+    #[test]
+    fn test_serialize_deserialize_monotonic_goes_forward() {
+        fn monotonic_ns_forwarded() -> i64 {
+            super::monotonic_ns() + 10_000 * 1_000_000_000
+        }
+        let timer = AtomicTimer::new(Duration::from_secs(5));
+        thread::sleep(Duration::from_secs(1));
+        let serialized = serde_json::to_string(&timer).unwrap();
+        let deserialized: AtomicTimer = serde_json::from_str(&serialized).unwrap();
+        let deserialized_rewinded = AtomicTimer::construct(
+            deserialized.duration,
+            deserialized.elapsed_ns(),
+            monotonic_ns_forwarded,
+        );
+        assert!(in_time_window(
+            deserialized_rewinded.elapsed(),
+            Duration::from_secs(1),
+            Duration::from_millis(100)
+        ));
+    }
+
+    #[test]
+    fn test_serialize_deserialize_monotonic_goes_backward() {
+        fn monotonic_ns_forwarded() -> i64 {
+            super::monotonic_ns() - 10_000 * 1_000_000_000
+        }
+        let timer = AtomicTimer::new(Duration::from_secs(5));
+        thread::sleep(Duration::from_secs(1));
+        let serialized = serde_json::to_string(&timer).unwrap();
+        let deserialized: AtomicTimer = serde_json::from_str(&serialized).unwrap();
+        let deserialized_rewinded = AtomicTimer::construct(
+            deserialized.duration,
+            deserialized.elapsed_ns(),
+            monotonic_ns_forwarded,
+        );
+        assert!(in_time_window(
+            deserialized_rewinded.elapsed(),
+            Duration::from_secs(1),
+            Duration::from_millis(100)
+        ));
+    }
+
+    #[test]
+    fn test_serialize_deserialize_monotonic_goes_zero() {
+        fn monotonic_ns_forwarded() -> i64 {
+            0
+        }
+        let timer = AtomicTimer::new(Duration::from_secs(5));
+        thread::sleep(Duration::from_secs(1));
+        let serialized = serde_json::to_string(&timer).unwrap();
+        let deserialized: AtomicTimer = serde_json::from_str(&serialized).unwrap();
+        let deserialized_rewinded = AtomicTimer::construct(
+            deserialized.duration,
+            deserialized.elapsed_ns(),
+            monotonic_ns_forwarded,
+        );
+        assert!(in_time_window(
+            deserialized_rewinded.elapsed(),
+            Duration::from_secs(1),
+            Duration::from_millis(100)
+        ));
+    }
+}
