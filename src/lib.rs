d67ce6a8a213b84a739b2f6d3ae454c4ff182480
Serhij S <s@g5.is>
2024-12-28 02:08:57 +0100

    permit handle expiration

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![ doc = include_str!( concat!( env!( "CARGO_MANIFEST_DIR" ), "/", "README.md" ) ) ]
 #![deny(missing_docs)]
 use std::{
-    sync::atomic::{AtomicI64, Ordering},
+    sync::atomic::{AtomicBool, AtomicI64, Ordering},
     time::Duration,
 };
 
@@ -11,6 +11,7 @@ use bma_ts::Monotonic;
 pub struct AtomicTimer {
     duration: AtomicI64,
     start: AtomicI64,
+    permit_handle_expiration: AtomicBool,
     monotonic_fn: fn() -> i64,
 }
 
@@ -20,11 +21,12 @@ fn monotonic_ns() -> i64 {
 
 impl AtomicTimer {
     #[allow(dead_code)]
-    fn construct(duration: i64, elapsed: i64, monotonic_fn: fn() -> i64) -> Self {
+    fn construct(duration: i64, elapsed: i64, phe: bool, monotonic_fn: fn() -> i64) -> Self {
         AtomicTimer {
             duration: AtomicI64::new(duration),
             start: AtomicI64::new(monotonic_fn() - elapsed),
             monotonic_fn,
+            permit_handle_expiration: AtomicBool::new(phe),
         }
     }
     /// Create a new atomic timer
@@ -33,24 +35,36 @@ impl AtomicTimer {
     ///
     /// Panics if the duration is too large (in nanos greater than `i64::MAX`)
     pub fn new(duration: Duration) -> Self {
-        AtomicTimer {
-            duration: AtomicI64::new(
-                duration
-                    .as_nanos()
-                    .try_into()
-                    .expect("Duration is too large"),
-            ),
-            start: AtomicI64::new(monotonic_ns()),
-            monotonic_fn: monotonic_ns,
-        }
+        Self::construct(
+            duration
+                .as_nanos()
+                .try_into()
+                .expect("Duration is too large"),
+            0,
+            true,
+            monotonic_ns,
+        )
     }
     /// Get the duration of the timer
     ///
     /// # Panics
     ///
     /// Panics if the duration is negative
+    #[inline]
     pub fn duration(&self) -> Duration {
         Duration::from_nanos(self.duration.load(Ordering::SeqCst).try_into().unwrap())
+    }
+    /// Similar to reset if expired but does not reset the timer. As the timer is checked for
+    /// expiration, a tiny datarace may occur despite it passes the tests well. As soon as the
+    /// timer is reset with any method, the flag is reset as well. If used in multi-threaded
+    /// environment, "true" is returned to a single worker only. After, the flag is reset.
+    #[inline]
+    pub fn permit_handle_expiration(&self) -> bool {
+        self.permit_handle_expiration
+            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |v| {
+                (v && self.expired()).then_some(false)
+            })
+            .is_ok()
     }
     /// Change the duration of the timer
     ///
@@ -64,21 +78,25 @@ impl AtomicTimer {
     /// Reset the timer
     #[inline]
     pub fn reset(&self) {
+        self.permit_handle_expiration.store(true, Ordering::SeqCst);
         self.start.store((self.monotonic_fn)(), Ordering::SeqCst);
     }
     /// Focibly expire the timer
+    #[inline]
     pub fn expire_now(&self) {
         self.start.store(
             (self.monotonic_fn)() - self.duration.load(Ordering::SeqCst),
             Ordering::SeqCst,
         );
     }
-    /// Reset the timer if it has expired, return true if reset
+    /// Reset the timer if it has expired, returns true if reset. If used in multi-threaded
+    /// environment, "true" is returned to a single worker only.
     #[inline]
     pub fn reset_if_expired(&self) -> bool {
         let now = (self.monotonic_fn)();
         self.start
             .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |start| {
+                self.permit_handle_expiration.store(true, Ordering::SeqCst);
                 (now.saturating_sub(start) >= self.duration.load(Ordering::SeqCst)).then_some(now)
             })
             .is_ok()
@@ -111,6 +129,7 @@ impl AtomicTimer {
             )
         }
     }
+    #[inline]
     fn elapsed_ns(&self) -> i64 {
         (self.monotonic_fn)().saturating_sub(self.start.load(Ordering::SeqCst))
     }
@@ -124,19 +143,27 @@ impl AtomicTimer {
 #[cfg(feature = "serde")]
 mod ser {
     use super::{monotonic_ns, AtomicTimer};
-    use serde::{ser::SerializeTuple as _, Deserialize, Deserializer, Serialize, Serializer};
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
     use std::sync::atomic::Ordering;
+
+    #[derive(Serialize, Deserialize)]
+    struct SerializedTimer {
+        duration: i64,
+        elapsed: i64,
+        phe: bool,
+    }
 
     impl Serialize for AtomicTimer {
         fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
         where
             S: Serializer,
         {
-            // serialize duration and elapsed into a tuple
-            let mut t = serializer.serialize_tuple(2)?;
-            t.serialize_element(&self.duration.load(Ordering::SeqCst))?;
-            t.serialize_element(&self.elapsed_ns())?;
-            t.end()
+            let s = SerializedTimer {
+                duration: self.duration.load(Ordering::SeqCst),
+                elapsed: self.elapsed_ns(),
+                phe: self.permit_handle_expiration.load(Ordering::SeqCst),
+            };
+            s.serialize(serializer)
         }
     }
 
@@ -145,9 +172,13 @@ mod ser {
         where
             D: Deserializer<'de>,
         {
-            // deserialize duration and elapsed from a tuple
-            let (duration, elapsed) = <(i64, i64)>::deserialize(deserializer)?;
-            Ok(AtomicTimer::construct(duration, elapsed, monotonic_ns))
+            let s = SerializedTimer::deserialize(deserializer)?;
+            Ok(AtomicTimer::construct(
+                s.duration,
+                s.elapsed,
+                s.phe,
+                monotonic_ns,
+            ))
         }
     }
 }
@@ -223,6 +254,46 @@ mod test {
         assert_eq!(result.len(), n);
         assert_eq!(result.into_iter().filter(|&v| v).count(), 1);
     }
+
+    #[test]
+    fn test_permit_handle_expiration() {
+        let timer = AtomicTimer::new(Duration::from_secs(1));
+        assert!(!timer.permit_handle_expiration());
+        thread::sleep(Duration::from_millis(1100));
+        assert!(timer.expired());
+        assert!(timer.permit_handle_expiration());
+        assert!(!timer.permit_handle_expiration());
+        timer.reset();
+        thread::sleep(Duration::from_millis(1100));
+        timer.reset();
+        assert!(!timer.permit_handle_expiration());
+    }
+
+    #[test]
+    fn test_permit_handle_expiration_no_datarace() {
+        let n = 1000;
+        let timer = Arc::new(AtomicTimer::new(Duration::from_millis(100)));
+        thread::sleep(Duration::from_millis(200));
+        assert!(timer.expired());
+        let barrier = Arc::new(Barrier::new(n));
+        let (tx, rx) = std::sync::mpsc::channel::<bool>();
+        let mut result = Vec::with_capacity(n);
+        for _ in 0..n {
+            let timer = timer.clone();
+            let barrier = barrier.clone();
+            let tx = tx.clone();
+            thread::spawn(move || {
+                barrier.wait();
+                tx.send(timer.permit_handle_expiration()).unwrap();
+            });
+        }
+        drop(tx);
+        while let Ok(v) = rx.recv() {
+            result.push(v);
+        }
+        assert_eq!(result.len(), n);
+        assert_eq!(result.into_iter().filter(|&v| v).count(), 1);
+    }
 }
 
 #[cfg(feature = "serde")]
@@ -230,7 +301,7 @@ mod test {
 mod test_serialization {
     use super::test::in_time_window;
     use super::AtomicTimer;
-    use std::{thread, time::Duration};
+    use std::{sync::atomic::Ordering, thread, time::Duration};
 
     #[test]
     fn test_serialize_deserialize() {
@@ -257,6 +328,7 @@ mod test_serialization {
         let deserialized_rewinded = AtomicTimer::construct(
             deserialized.duration().as_nanos().try_into().unwrap(),
             deserialized.elapsed_ns(),
+            deserialized.permit_handle_expiration.load(Ordering::SeqCst),
             monotonic_ns_forwarded,
         );
         assert!(in_time_window(
@@ -278,6 +350,7 @@ mod test_serialization {
         let deserialized_rewinded = AtomicTimer::construct(
             deserialized.duration().as_nanos().try_into().unwrap(),
             deserialized.elapsed_ns(),
+            deserialized.permit_handle_expiration.load(Ordering::SeqCst),
             monotonic_ns_forwarded,
         );
         assert!(in_time_window(
@@ -299,6 +372,7 @@ mod test_serialization {
         let deserialized_rewinded = AtomicTimer::construct(
             deserialized.duration().as_nanos().try_into().unwrap(),
             deserialized.elapsed_ns(),
+            deserialized.permit_handle_expiration.load(Ordering::SeqCst),
             monotonic_ns_forwarded,
         );
         assert!(in_time_window(
